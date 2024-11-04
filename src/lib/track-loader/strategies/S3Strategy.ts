549cec8958664c131224b970838ac706c3b31dd2
Lyoubomir Katzarov <lyoubomir.katzarov@gmail.com>
2024-11-04 19:36:01 +0100

    S3 presigned url endpoint - don't subscribe when making the req.

--- src/lib/track-loader/strategies/S3Strategy.ts
+++ src/lib/track-loader/strategies/S3Strategy.ts
@@ -5,14 +5,14 @@ import { AbstractFetchingStrategy } from "../TrackLoader";
 
 export class S3Strategy implements AbstractFetchingStrategy {
   async fetchTrack(uri: string) {
-    // https://redux-toolkit.js.org/rtk-query/usage/usage-without-react-hooks
     const promise = store.dispatch(
-      tracksApi.endpoints.getS3PresignedUrlForTrack.initiate(uri)
+      tracksApi.endpoints.getS3PresignedUrlForTrack.initiate(uri, {
+        subscribe: false,
+        forceRefetch: true,
+      })
     );
-    promise.refetch();
 
     const { data, isSuccess, error } = await promise;
-    promise.unsubscribe();
 
     if (isSuccess) {
       const blob = await nonRTKQueryApi.fetchTrackFromS3Bucket(data.url);
